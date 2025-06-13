cd70961c7673dba0b8dbdd38542a978c19249ddf
DnyaneshwarKalwale <prembkalwale@gmail.com>
2025-06-13 11:47:43 +0530

    bugs fix

--- backend/src/routes/paymentRoutes.js
+++ backend/src/routes/paymentRoutes.js
@@ -1,22 +1,15 @@
 const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
 const {
-  getUserPaymentHistory,
-  getTransactionById,
-  getAllTransactions,
-  getUserSpendingSummary,
-  getUserPaymentMethods,
-  setDefaultPaymentMethod,
-  downloadInvoice,
-  downloadBillingHistory,
   getPaymentMethods,
   addPaymentMethod,
-  getPaymentHistory
+  setDefaultPaymentMethod,
+  getPaymentHistory,
+  downloadInvoice
 } = require('../controllers/paymentController');
-const { protect, isAdmin } = require('../middleware/authMiddleware');
 
-const router = express.Router();
-
-// Protect all routes
+// All routes are protected
 router.use(protect);
 
 // Payment methods routes
@@ -26,13 +19,6 @@ router.put('/methods/:id/default', setDefaultPaymentMethod);
 
 // Payment history routes
 router.get('/history', getPaymentHistory);
-router.get('/history/download', downloadBillingHistory);
 router.get('/invoices/:id/download', downloadInvoice);
-
-// Admin only routes
-router.use(isAdmin);
-router.get('/transactions', getAllTransactions);
-router.get('/transactions/:id', getTransactionById);
-router.get('/spending-summary', getUserSpendingSummary);
 
 module.exports = router; 
